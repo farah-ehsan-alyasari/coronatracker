088355942cdfa25fdd7a80b2e3596db83a885aa1
farah-ehsan-alyasari <farah.alyasari@yahoo.com>
2023-03-20 12:53:49 -0700

    first commit

---
+++ src/main/java/com/example/cornoatracker/CornoatrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cornoatracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CornoatrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CornoatrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cornoatracker/CornoatrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cornoatracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CornoatrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
